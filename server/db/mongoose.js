c8b765d6425c7f72e5ad8907af974f3612d05cfd
Yudiz Solutions <yudizsolutions@Yudiz-LEET.local>
2018-03-14 17:54:09 +0530

    Delete Api added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
-if(process.env){
+//console.log(process.env.USER != 'yudizsolutions');
+if(process.env.USER != 'yudizsolutions'){
     mongoose.connect('mongodb://leet1337:leet123@ds113019.mlab.com:13019/todoapp');
 }else{
     mongoose.connect('mongodb://localhost:27017/TodoApp');
