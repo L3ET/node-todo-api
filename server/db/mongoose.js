3f2ae4289ceb64a26123de58070ae35feb0c62a3
Yudiz Solutions <yudizsolutions@Yudiz-LEET.local>
2018-03-14 16:22:53 +0530

    Mongo url change

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://leet1337:leet123@ds113019.mlab.com:13019/todoapp');
-
+if(process.env){
+    mongoose.connect('mongodb://leet1337:leet123@ds113019.mlab.com:13019/todoapp');
+}else{
+    mongoose.connect('mongodb://localhost:27017/TodoApp');
+}
 module.exports = {
     mongoose
 };
