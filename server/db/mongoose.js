33cb44ce80350cea7ab7de1c7edcdb5dc9ebf17e
Yudiz Solutions <yudizsolutions@Yudiz-LEET.local>
2018-03-19 11:22:42 +0530

    mongoose change rollback

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 if(process.env.USER != 'yudizsolutions'){
     mongoose.connect('mongodb://leet1337:leet123@ds113019.mlab.com:13019/todoapp');
 }else{
-    mongoose.connect('mongodb://leet1337:leet123@ds113019.mlab.com:13019/todoapp');
+    mongoose.connect('mongodb://localhost:27017/TodoApp');
 }
 module.exports = {
     mongoose
